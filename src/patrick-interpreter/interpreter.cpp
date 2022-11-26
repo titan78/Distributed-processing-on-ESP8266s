5ec78d48e68e5ae9d5acc4eba3a394fdee67603d
unknown <support@iparses.ir>
2022-11-26 17:29:40 +0330

    interpreter initialization 2

--- src/patrick-interpreter/interpreter.cpp
+++ src/patrick-interpreter/interpreter.cpp
@@ -10,7 +10,7 @@ Interpreter::Interpreter(const std::string &fileName)
  *                  declare variable
  *
  *  =================================================== */
-
+#if defined(DECLARE_VARIABLE)
 struct Interpreter::DeclarableValue {
 private:
     std::string type;
@@ -18,9 +18,17 @@ private:
     std::string value = "";
     ScopeLevel scope;
 public:
-    DeclarableValue(const std::string& line);
-    Value toValue(const ScopeLevel& scope);
-    std::string getName() const;
+    DeclarableValue(const std::string& line) {
+
+    }
+
+    Value toValue(const ScopeLevel& scope){
+
+    }
+
+    std::string getName() const {
+
+    }
 };
 
 bool Interpreter::declareVariable(const std::string &line)
@@ -36,18 +44,21 @@ bool Interpreter::declareVariable(const std::string &line)
     } catch (...) {
 
         result = false;
+        //exception handler
 
     }
 
     return result;
 }
+#endif
+
 
 /** ===================================================
  *
  *                  declare function
  *
  *  =================================================== */
-
+#if defined(DECLARE_FUNCTION)
 struct Interpreter::FunctionType {
 private:
     size_t end;
@@ -66,4 +77,82 @@ bool Interpreter::declareFunction(const std::string &line)
 {
 
 }
+#endif
 
+/** ===================================================
+ *
+ *                  run command
+ *
+ *  =================================================== */
+#if defined(RUN_COMMAND)
+bool Interpreter::runCommand(const std::string &line)
+{
+    bool executionReult = true;
+    const auto index = line.find(':');
+
+    if(index == std::string::npos){ //expression
+
+        //executionReult = solveExpression(line);
+
+    } else { //keyword
+        const std::string keyword = line.substr(0, index);
+        const std::string command = line.substr(index);
+
+        switch (keywordStringTokeywordEnum(keyword)) {
+        case KWS::BranchTo: break;
+        case KWS::Declare: executionReult = declareVariable(command); break;
+        case KWS::Else: break;
+        case KWS::If: break;
+        case KWS::Function: break;
+        case KWS::Loop: break;
+        case KWS::Return: break;
+        case KWS::Syscall: break;
+
+        default:
+            executionReult = false;
+            invalidKeywordException();
+            break;
+        }
+    }
+
+    return executionReult;
+}
+#endif
+
+/** ===================================================
+ *
+ *                  PCB manager
+ *
+ *  =================================================== */
+#if defined(PCB_MANAGER)
+struct Interpreter::PCB{
+    ScopeLevel scope;
+};
+
+void Interpreter::pcbManager()
+{
+
+}
+#endif
+
+Interpreter::DTS Interpreter::stringTypeToEnumType(const std::string &type)
+{
+    std::map<std::string, DTS> dataTypes =
+        {{"int", DTS::Int}, {"lint", DTS::LInt}, {"llint", DTS::LLInt}, {"uint", DTS::UInt}, {"F32", DTS::F32}, {"F64", DTS::F64}};
+
+    return dataTypes[type];
+}
+
+Interpreter::KWS Interpreter::keywordStringTokeywordEnum(const std::string &type)
+{
+    std::map<std::string, KWS> keywords =
+        {{"declare", KWS::Declare}, {"branch_to", KWS::BranchTo}, {"function", KWS::Function},
+         {"return", KWS::Return}, {"if", KWS::If}, {"Else", KWS::Else}, {"loop", KWS::Loop}, {"syscall", KWS::Syscall}};
+
+    return keywords[type];
+}
+
+void Interpreter::invalidKeywordException() noexcept
+{
+
+}
--- src/patrick-interpreter/interpreter.h
+++ src/patrick-interpreter/interpreter.h
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <map>
 #include <any>
+#include <stack>
 
 class Interpreter
 {
@@ -26,8 +27,12 @@ private: // types
     typedef std::pair<ScopeLevel, TypedValue> Value;
 
     /// @brief data types
-    enum class DTS{InValid = 0x0, Int, LInt, LLInt, UInt, F32, F64};
+    enum class DTS{InValid, Int, LInt, LLInt, UInt, F32, F64};
 
+    /// @brief keywords
+    enum class KWS{Declare, Syscall, If, Else, Loop, Return, BranchTo, Function};
+
+    #define DECLARE_VARIABLE
     /**
      * @brief The DeclarableValue class; The fields of this structure are filled by the fromString function
      *  from the lines of code related to the variable declaration,
@@ -35,6 +40,7 @@ private: // types
      */
     struct DeclarableValue;
 
+    #define DECLARE_FUNCTION
     /**
      * @brief The FunctionType class; Each function, when implemented, gets an instance of this structure,
      *  which holds the beginning and end of the function definition in the code.
@@ -44,32 +50,102 @@ private: // types
      */
     struct FunctionType;
 
+    #define PCB_MANAGER
+    /**
+     * @brief process control block
+     */
+    struct PCB;
+
+    struct calculationMetaData {
+        std::string type;
+        int index;
+    };
+
 private: // members
+#ifdef DECLARE_VARIABLE
     bool declareVariable(const std::string& line);
+#endif
+
+    //#define ACCESS_VARIABLE
     //accessVariable()
+
+    //#define CHANGE_VARIABLE
     //changeVariable()
+
+    //#define CHECK_SCOPE_COMPATIBILITY
     //checkScopeCompatiblity()
+
+    //#define CHECK_DATA_TYPE
     //checkDataType()
+
+    //#define MATH_OPERATION
     //mathOperation()
-    //if-else blocks
+
+    //#define IF_ELSE_BLOCK
+    //ifElseBlock()
+
+    //#define LOOP_BLOCK
     //loop()
+
+#ifdef DECLARE_FUNCTION
     bool declareFunction(const std::string& line);
+#endif
+
+    //#define INVOKE_FUNCTION
     //invokeFunction()
+
+    //#define STORE_FUNCTION_RETURN_VALUE
     //storeFunctionReturnValue()
+
+    //#define BRANCH_TO
     //branchTo()
+
+    //#define SOLVE_EXPRESSION
+    //solveExpression()
+
+//    #define DECLARE_FUNCTION
     /*cloudLinker() --> Due to the lack of resources in ESPs, we do not have the ability to precompile,
      *                  so we have to create a cloud link between calls and implementations of functions. */
 
+    #define RUN_COMMAND
     bool runCommand(const std::string& line);
+
+#ifdef PCB_MANAGER
+    void pcbManager();
+#endif
+
+
+private: // helper memebers
+    DTS stringTypeToEnumType(const std::string& type);
+    KWS keywordStringTokeywordEnum(const std::string& type);
+
+
+private: // exceptions
+    void invalidKeywordException() noexcept;
 
 private: // variables
     /// first: name of value, second: Value Pair
     std::map<std::string, Value> _values;
     std::map<std::string, FunctionType> _functions;
 
+    /// calclulation stacks
+    std::stack<int> intStack;
+    std::stack<long> lintStack;
+    std::stack<long long> llintStack;
+    std::stack<unsigned int> uintStack;
+    std::stack<float> f32Stack;
+    std::stack<double> f64Stack;
+    std::map<std::string, calculationMetaData> metadata;
+    std::stack<std::string> calculationStack;
+//    std::stack<long double> f128Stack;
+
 private: // constants
-    std::map<std::string, DTS> _dataTypes =
-        {{"int", DTS::Int}, {"lint", DTS::LInt}, {"llint", DTS::LLInt}, {"uint", DTS::UInt}, {"F32", DTS::F32}, {"F64", DTS::F64}};
+//    std::map<std::string, DTS> _dataTypes =
+//        {{"int", DTS::Int}, {"lint", DTS::LInt}, {"llint", DTS::LLInt}, {"uint", DTS::UInt}, {"F32", DTS::F32}, {"F64", DTS::F64}};
+
+//    std::map<std::string, KWS> _keywords =
+//        {{"declare", KWS::Declare}, {"branch_to", KWS::BranchTo}, {"function", KWS::Function},
+//         {"return", KWS::Return}, {"if", KWS::If}, {"Else", KWS::Else}, {"loop", KWS::Loop}, {"syscall", KWS::Syscall}};
 
 };
 
